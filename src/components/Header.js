35bbd1605c015ce73dc1bac3d8f0c2542d045d3c
Shivshankar2246 <shivshankar2246@gmail.com>
2023-03-02 17:55:43 +0530

    shopping cart numbers added

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,17 +30,22 @@ function Header() {
 
         {/* right side */}
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-          <div className="cursor-pointer">
+          <div className=" link ">
             <p>Hello Shiva</p>
-            <p>Acount & Lists</p>
+            <p className="font-extrabold md:text-sm">Acount & Lists</p>
           </div>
-          <div className="cursor-pointer">
+          <div className=" link">
             <p>Returns</p>
-            <p>& Orders</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
-          <div className="cursor-pointer">
+          <div className=" relative link flex items-center ">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+              0
+            </span>
             <ShoppingCartIcon className="h-10" />
-            <p>Basket</p>
+            <p className="hidden md:inline font-extrabold md:text-sm mt-2">
+              Basket
+            </p>
           </div>
         </div>
       </div>
