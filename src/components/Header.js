9114a32799ba83e8690d439f209de415c15daa76
Shivshankar2246 <shivshankar2246@gmail.com>
2023-03-02 17:21:46 +0530

    header completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
   return (
     <header>
       {/* top nav */}
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
-        <div className="mt-2 items-center flex-grow sm:flex-grow-0">
+        <div className="mt-2 items-center flex-grow sm:flex-grow-0 mt-5">
           <Image
             src="https://links.papareact.com/f90"
             width={150}
@@ -13,6 +18,30 @@ function Header() {
             objectFit="contain"
             className="cursor-pointer"
           />
+        </div>
+        {/* search bar */}
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer ml-3   bg-yellow-400 hover:bg-yellow-500">
+          <input
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4 "
+            type="text"
+          />
+          <SearchIcon className="h-12  p-4" />
+        </div>
+
+        {/* right side */}
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="cursor-pointer">
+            <p>Hello Shiva</p>
+            <p>Acount & Lists</p>
+          </div>
+          <div className="cursor-pointer">
+            <p>Returns</p>
+            <p>& Orders</p>
+          </div>
+          <div className="cursor-pointer">
+            <ShoppingCartIcon className="h-10" />
+            <p>Basket</p>
+          </div>
         </div>
       </div>
     </header>
