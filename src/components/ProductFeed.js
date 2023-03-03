eba082545b23eafd9f71766d82180c141715575d
Shivshankar2246 <shivshankar2246@gmail.com>
2023-03-03 15:34:04 +0530

    product components added

---
+++ src/components/ProductFeed.js
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import Product from "./Product";
+
+function ProductFeed({ products }) {
+  return (
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto">
+      {products
+        .slice(0, 4)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+      <img className="md:col-span-full" src="http://links.papareact.com/dyz" />
+
+      {products
+        .slice(4, products.length)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+    </div>
+  );
+}
+export default ProductFeed;
